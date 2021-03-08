7f8fa3cb99aad63e413ffbfc810d74715d23c9af
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-03-08 14:55:32 -0300

    document content

--- src/mindustry/content.ts
+++ src/mindustry/content.ts
@@ -1,4 +1,13 @@
+/** An abstract top class representing in game content
+ *
+ * This class should not be instantiated
+ */
 export abstract class Content {}
+
+/** An abstract top class representing idetifiable in game content
+ *
+ * This class should not be instantiated
+ */
 export abstract class MappableContent extends Content {
   constructor(public name: string) {
     super()
@@ -8,4 +17,9 @@ export abstract class MappableContent extends Content {
     return this.name
   }
 }
+
+/** An abstract top class representing unlockable in game content
+ *
+ * This class should not be instantiated
+ */
 export abstract class UnlockableContent extends MappableContent {}
