36b6ee1d37e77872f3fd5b3ffdfa3c3c31f1106e
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-03-08 13:39:45 -0300

    document StreamedDataView

--- src/streamed_data_view.ts
+++ src/streamed_data_view.ts
@@ -3,72 +3,123 @@
  * A mix of `DataView` and the `DataInputStream` java class
  */
 export default class StreamedDataView {
+  /**
+   * The `DataView` wrapped by this `StreamedDataView`.
+   *
+   * This is meant to be exposed as a readonly property, writes to the buffer may cause undefined behaviour
+   */
   readonly data: DataView
 
   constructor(public readonly buffer: ArrayBuffer) {
     this.data = new DataView(buffer)
   }
 
+  /**
+   * Internal byte offset of this `StreamedDataView`
+   */
   private currentOffset = 0
 
+  /**
+   * The current byte offset of this `StreamedDataView`
+   */
   get offset(): number {
     return this.currentOffset
   }
 
+  /**
+   * Reads the next 4 bytes as a 32-bit float value. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getFloat32(littleEndian?: boolean): number {
     const value = this.data.getFloat32(this.currentOffset, littleEndian)
     this.currentOffset += 4
     return value
   }
 
+  /**
+   * Reads the next 8 bytes as a 64-bit float value. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getFloat64(littleEndian?: boolean): number {
     const value = this.data.getFloat64(this.currentOffset, littleEndian)
     this.currentOffset += 8
     return value
   }
 
+  /**
+   * Reads the next byte as a 8-bit int. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getInt8(): number {
     const value = this.data.getInt8(this.currentOffset)
     this.currentOffset++
     return value
   }
 
+  /**
+   * Reads the next 2 bytes as a 16-bit int. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getInt16(littleEndian?: boolean): number {
     const value = this.data.getInt16(this.currentOffset, littleEndian)
     this.currentOffset += 2
     return value
   }
 
+  /**
+   * Reads the next 4 bytes as a 32-bit int. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getInt32(littleEndian?: boolean): number {
     const value = this.data.getInt32(this.currentOffset, littleEndian)
     this.currentOffset += 4
     return value
   }
 
+  /**
+   * Reads the next byte as a 8-bit unsigned int. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getUint8(): number {
     const value = this.data.getUint8(this.currentOffset)
     this.currentOffset++
     return value
   }
 
+  /**
+   * Reads the next 2 bytes as a 16-bit unsigned int. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getUint16(littleEndian?: boolean): number {
     const value = this.data.getUint16(this.currentOffset, littleEndian)
     this.currentOffset += 2
     return value
   }
 
+  /**
+   * Reads the next 4 bytes as a 32-bit unsigned int. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getUint32(littleEndian?: boolean): number {
     const value = this.data.getUint32(this.currentOffset, littleEndian)
     this.currentOffset += 4
     return value
   }
 
+  /**
+   * Reads the next 8 bytes as a 64-bit int. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getBigInt64(littleEndian?: boolean): bigint {
     const value = this.data.getBigInt64(this.currentOffset, littleEndian)
     this.currentOffset += 8
     return value
   }
 
+  /**
+   * Reads the next 8 bytes as a 64-bit unsigned int. There is
+   * no alignment constraint; multi-byte values may be fetched from any offset.
+   */
   getBigUint64(littleEndian?: boolean): bigint {
     const value = this.data.getBigUint64(this.currentOffset, littleEndian)
     this.currentOffset += 8
@@ -82,6 +133,11 @@ export default class StreamedDataView {
     return String.fromCodePoint(this.getUint8())
   }
 
+  /**
+   * Reads a string that has been encoded using a
+   * modified UTF-8
+   * format.
+   */
   getString(): string {
     const utflen = this.getUint16()
     let c = 0,
@@ -142,6 +198,9 @@ export default class StreamedDataView {
     return result
   }
 
+  /**
+   * Reads the next byte as a boolean
+   */
   getBool(): boolean {
     const value = this.getInt8()
     if (value < 0) throw new Error('Bat byte input')
