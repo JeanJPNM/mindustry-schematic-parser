20e61723d9d8a6c9808cd81e39cc422075706f60
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-03-08 13:51:15 -0300

    document schematics

--- src/schematic.ts
+++ src/schematic.ts
@@ -1,14 +1,35 @@
 import { Block, PowerGenerator } from './mindustry/block'
 import { ItemCode, ItemCost } from './mindustry/item'
-
+/**
+ * A simple representation for a mindustry schematic
+ */
 export default class Schematic {
   constructor(
+    /**
+     * The tiles that compose this schematic
+     */
     public tiles: SchematicTile[],
+    /**
+     * These tags contain the schematic metadata (like its name and description)
+     */
     public tags: Map<string, string>,
+    /**
+     * With of the schematic in tiles
+     */
     public width: number,
+    /**
+     * Height of the schematic in tiles
+     */
     public height: number
   ) {}
 
+  /**
+   * The amount of power this schematic can produce
+   *
+   * This is a separated measurement that does not interfere with `powerConsumption`
+   *
+   * This measurement may vary if there is an `OverdriveProjector` or an `OverdriveDome` contained in this schematic
+   */
   get powerProduction(): number {
     let result = 0
     for (const tile of this.tiles) {
@@ -18,6 +39,13 @@ export default class Schematic {
     return result
   }
 
+  /**
+   * The amount of power this schematic consumes to work properly
+   *
+   * This is a separated measurement that does not interfere with `powerConsumption`
+   *
+   * This measurement may vary if there is an `OverdriveProjector` or an `OverdriveDome` contained in this schematic
+   */
   get powerConsumption(): number {
     let result = 0
     for (const tile of this.tiles) {
@@ -26,6 +54,9 @@ export default class Schematic {
     return result
   }
 
+  /**
+   * The items needed to build this schematic
+   */
   get requirements(): ItemCost {
     const requirements: ItemCost = {}
     for (const tile of this.tiles) {
@@ -45,10 +76,25 @@ export default class Schematic {
 }
 export class SchematicTile {
   constructor(
+    /**
+     * The block occupying this tile
+     */
     public block: Block,
+    /**
+     * The x coordinate of this tile
+     */
     public x: number,
+    /**
+     * The y coordinate of this tile
+     */
     public y: number,
+    /**
+     * The configuration of this tile (varies according to the block), may be `undefined` or `null`
+     */
     public config: unknown,
+    /**
+     * The rotation of this tile
+     */
     public rotation: number
   ) {}
 }
