05de2f74cf95af04d85d13e202604895a0637d57
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-03-11 08:39:43 -0300

    add test for the Point2 class

---
+++ src/arc/point2.test.ts
@@ -0,0 +1,13 @@
+import { Point2 } from './point2'
+
+test('pack coordinates to an int', () => {
+  const point = new Point2(10, 10)
+  const packed = point.pack()
+  const unpack = Point2.unpack(packed)
+  expect(point.equals(unpack)).toBe(true)
+})
+test('unpack numbers from other Point2', () => {
+  const original = new Point2(16, 64)
+  expect(Point2.x(original.pack())).toBe(16)
+  expect(Point2.y(original.pack())).toBe(64)
+})
