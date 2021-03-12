661392cae7b4f9a8dd5b932a9114138c5e43ae45
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-03-12 19:33:56 -0300

    add colors to items

--- src/mindustry/item/item.ts
+++ src/mindustry/item/item.ts
@@ -1,10 +1,36 @@
 import { ItemName } from './item_name'
 
+type ItemColorMap = {
+  [x in ItemName]: string
+}
+const itemColors: ItemColorMap = {
+  copper: '#d99d73',
+  lead: '#8c7fa9',
+  metaglass: '#ebeef5',
+  graphite: '#b2c6d2',
+  sand: '#f7cba4',
+  coal: '#272727',
+  titanium: '#8da1e3',
+  thorium: '#f9a3c7',
+  scrap: '#777777',
+  silicon: '#53565c',
+  plastanium: '#cbd97f',
+  'phase-fabric': '#f4ba6e',
+  'surge-alloy': '#f3e979',
+  'spore-pod': '#7457ce',
+  'blast-compound': '#ff795e',
+  pyratite: '#ffaa5f',
+}
+
 /** A wrapper for `ItemCode`, can be useful with `instanceof` */
 export class Item {
   private static itemMap: Map<ItemName, Item> = new Map()
 
-  private constructor(public readonly code: ItemName) {}
+  readonly color: string
+
+  private constructor(public readonly code: ItemName) {
+    this.color = itemColors[code]
+  }
 
   static create(name: ItemName): Item {
     let item = this.itemMap.get(name)
