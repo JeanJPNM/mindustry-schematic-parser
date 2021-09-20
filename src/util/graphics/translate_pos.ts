57afcc8898ad062f4d20976bc85b71fcd9dc2596
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-09-20 11:57:14 -0300

    refactor translatePos

--- src/util/graphics/translate_pos.ts
+++ src/util/graphics/translate_pos.ts
@@ -5,16 +5,13 @@ export function translatePos(
   tile: SchematicTile,
   canvas: Canvas
 ): { x: number; y: number } {
-  let { x, y } = tile
-  y++
+  const { x, y } = tile
   const { size } = tile.block
+  const offsetX = -Math.ceil(size / 2) + 1
+  const offsetY = Math.floor(size / 2) + 1
 
-  // "fix" coordinates
-  x -= Math.floor(size / 2 - 0.1)
-  y += Math.floor(size / 2)
-
-  y *= 32
-  x *= 32
-  y = canvas.height - y
-  return { x, y }
+  return {
+    x: (x + offsetX) * 32,
+    y: canvas.height - (y + offsetY) * 32,
+  }
 }
