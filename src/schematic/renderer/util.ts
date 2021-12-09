9c493dd05dc30ef6c40080dd41f2e93c33798759
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-12-09 14:48:29 -0300

    remove unused `mapTiles` function

--- src/schematic/renderer/util.ts
+++ src/schematic/renderer/util.ts
@@ -1,6 +1,5 @@
 import { BlockOutputDirection } from '../../mindustry/block/block'
 import { Flags } from '../../util'
-import { Schematic } from '../schematic'
 import { SchematicTile } from '../tile'
 
 export function handlePlacement(tile: SchematicTile): { x: number; y: number } {
@@ -13,27 +12,6 @@ export function handlePlacement(tile: SchematicTile): { x: number; y: number } {
   }
 }
 export type SchematicTileMap = ((SchematicTile | undefined)[] | undefined)[]
-export function mapTiles(schematic: Schematic): SchematicTileMap {
-  const { width } = schematic
-  const result: SchematicTile[][] = []
-  for (let x = 0; x < width; x++) {
-    result[x] = []
-  }
-  for (const tile of schematic.tiles) {
-    const { size } = tile.block
-    const start = handlePlacement(tile)
-    const end = {
-      x: start.x + size,
-      y: start.y + size,
-    }
-    for (let { x } = start; x < end.x && x < schematic.width; x++) {
-      for (let { y } = start; y < end.y && y < schematic.height; y++) {
-        result[x][y] = tile
-      }
-    }
-  }
-  return result
-}
 
 const directions = [
   BlockOutputDirection.front,
